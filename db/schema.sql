5f41e36f576727d4dd1902d762d80ef76113c12b
lauraeschooley <lauraeschooley@gmail.com>
2019-05-19 22:12:46 -0700

    folder structure and files created

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+drop database if exists burgers_db;
+create database burgers_db;
+use burgers_db;
+
+create table burgers 
+(
+id integer auto_increment not null,
+burger_name varchar (255) not null,
+devoured boolean default false,
+primary key (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+use burgers_db;
+
+insert into burgers (burger_name) values ("double double");
+insert into burgers (burger_name) values ("ultimate bacon burger");
+insert into burgers (burger_name) values ("deadpool burger");
